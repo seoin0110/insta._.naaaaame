16845cd36e618ef6946c16b61051bcbeb96f7dd8
김서인 <79834222+seoin0110@users.noreply.github.com>
2023-08-19 02:23:36 +0900

    feat: add openai code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
 
 function App() {
+  const [music, setMusic] = useState({ title: "", singer: "" });
+
+  // OpenAI API 호출
+  const fetchOpenApi = useCallback(() => {
+    const configuration = new Configuration({
+      apiKey: process.env.REACT_APP_OPENAI_KEY,
+    });
+
+    const testPrompt = "recommend me one female indie song";
+
+    new OpenAIApi(configuration)
+      .createCompletion({
+        model: "text-davinci-003",
+        prompt: testPrompt,
+        temperature: 0,
+        max_tokens: 150,
+      })
+      .then((res) => {
+        const { choices } = res.data;
+        const [title, singer] = choices[0].text.split("by");
+
+        setMusic({ title, singer }); // 음악의 제목, 가수 데이터 저장
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchOpenApi(); // Mount 시 호출한다.
+  }, []);
+
+  const { title, singer } = music;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {title} - {singer}
     </div>
   );
 }
