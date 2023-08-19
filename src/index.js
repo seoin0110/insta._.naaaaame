092539bfed6eb3a8ff69623e4325aef9e8a9ab37
eatingrabbit <lhy100494@gmail.com>
2023-08-19 14:24:51 +0900

    feat: add openai api handler GPT4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import MainPage from "./pages/MainPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<MainPage />);
 
 reportWebVitals();
