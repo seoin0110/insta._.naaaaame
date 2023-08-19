71de9461aeb214f58d7d5b0fb64fe7763678c96d
김서인 <79834222+seoin0110@users.noreply.github.com>
2023-08-19 13:06:10 +0900

    feat: add prompt

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function App() {
   const [isReady, setIsReady] = useState(false);
 
   // const api_key=""
-  const messages = [
-    { role: "system", content: "You are a helpful assistant." },
-    { role: "user", content: "서강대에 대해서 설명해줘" },
-  ];
+  const messages = [{ role: "user", content: "서강대에 대해서 설명해줘" }];
   const headers = {
     "Content-Type": "application/json",
     Authorization: "Bearer " + process.env.REACT_APP_OPENAI_KEY,
@@ -29,7 +26,7 @@ function App() {
         "https://api.openai.com/v1/chat/completions",
         JSON.stringify({
           messages: messages,
-          model: "gpt-3.5-turbo",
+          model: "gpt-4",
         }),
         { headers }
       )
@@ -44,24 +41,10 @@ function App() {
       });
   };
 
-  handleSubmit();
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleSubmit}>눌러보세요</button>
+      {isReady ? resultText : "로딩중..."}
     </div>
   );
 }
